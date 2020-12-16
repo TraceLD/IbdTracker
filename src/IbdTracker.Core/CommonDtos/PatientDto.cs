bee987ed6b7e1ca91925896066b5425ef4f8568d
Trace <lukaszdabrowski881@gmail.com>
2020-12-16 16:00:34 +0100

    add Dto for GET Patient response

---
+++ src/IbdTracker.Core/CommonDtos/PatientDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IbdTracker.Core.CommonDtos
+{
+    public class PatientDto
+    {
+        public int PatientId { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateDiagnosed { get; set; }
+    }
+}
