144c2e5f39a78d45bd19de3e225a4fbe8c8d29a6
Trace <lukaszdabrowski881@gmail.com>
2020-12-16 16:00:18 +0100

    add design time factory
    so that the connection string doesn't have to be hardcoded for migrations

---
+++ src/IbdTracker.Core/IbdSymptomTrackerContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using IbdTracker.Core.Config;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace IbdTracker.Core
+{
+    public class IbdSymptomTrackerContextFactory : IDesignTimeDbContextFactory<IbdSymptomTrackerContext>
+    {
+        public IbdSymptomTrackerContext CreateDbContext(string[] args)
+        {
+            // build config;
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../IbdTracker"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            // get connection string;
+            var optionsBuilder = new DbContextOptionsBuilder<IbdSymptomTrackerContext>();
+            var boundConfig = config.GetSection("Database").Get<DbConfig>();
+            optionsBuilder
+                .UseNpgsql(boundConfig.ConnectionString, b => b.MigrationsAssembly("IbdTracker.Core"));
+            return new IbdSymptomTrackerContext(optionsBuilder.Options);
+        }
+    }
+}
