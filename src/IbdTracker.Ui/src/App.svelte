69103059e073bf92fb8352b2b6622d1eef9648f4
Trace <lukaszdabrowski881@gmail.com>
2020-12-16 08:24:38 +0100

    if result is success show it

--- src/IbdTracker.Ui/src/App.svelte
+++ src/IbdTracker.Ui/src/App.svelte
@@ -34,15 +34,17 @@
 
 	async function handleApiClick() {
 		const accessToken = await auth0.getTokenSilently();
-		console.log("Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IndiUHRYTUhGVHZsSUZKblBGNGhRdyJ9.eyJpc3MiOiJodHRwczovL3RyYWNlbGQuZXUuYXV0aDAuY29tLyIsInN1YiI6IkhDbkZHa05tVmhyTUFTTGZlM0laRVVxQ24xMzl5c1Y4QGNsaWVudHMiLCJhdWQiOiJodHRwczovL2liZHN5bXB0b210cmFja2VyLmNvbS9hcGkiLCJpYXQiOjE2MDgwMjA0MjcsImV4cCI6MTYwODEwNjgyNywiYXpwIjoiSENuRkdrTm1WaHJNQVNMZmUzSVpFVXFDbjEzOXlzVjgiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.k1l1sR7DDLKpFwHTx5cPzXlXz6D0wv2LMVT3ttXrTxBPsfSn41AKCyc1CBRech6Vr6g5lucJyfdT8bBbNlAk-yC7Vk2q4AeC9KcrZgp7jhXnws8f8QzXutgka86blT7gu_9RjXYgHJUtm8LyUXnTcdvivkAti_2pH8McTAnMQtMYj85A_mSXnRNW10ZpNNRlftPXJGZdRHtPvQSFhhL6rRqq5KyHYxmGxU9bsaOhJ6DrDbojCWdz0oyYgqMlA0ncwvT5U2QyfPrsTtr6UpDj_lA05FwkAqQ1q4UHb37jxfAu6bR59A_j-xXWPO2XEck-xq-opyUwo5iJQMCSZj_RHw");
 		const result = await fetch('http://localhost:8080/api/private', {
 			method: 'GET',
 			headers: {
 				Authorization: "Bearer " + accessToken
 			}
 		});
-		const data = await result.json();
-		console.log(data);
+
+		if (result.ok) {
+			const data = await result.json();
+			console.log(data);
+		}	
 	}
 </script>
 
