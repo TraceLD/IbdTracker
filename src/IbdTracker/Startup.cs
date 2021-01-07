fba76ca95072c8880d79186922b803c82ff59671
Trace <lukaszdabrowski881@gmail.com>
2021-01-07 17:36:39 +0000

    add CORS for development purposes

--- src/IbdTracker/Startup.cs
+++ src/IbdTracker/Startup.cs
@@ -21,12 +21,14 @@ namespace IbdTracker
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -77,16 +79,29 @@ namespace IbdTracker
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "IbdTracker", Version = "v1"});
             });
+
+            if (Environment.IsDevelopment())
+            {
+                services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(
+                        builder =>
+                        {
+                            builder.WithOrigins("http://localhost:8080");
+                        });
+                });
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IbdTracker v1"));
+                app.UseCors();
             }
 
             app.UseHttpsRedirection();
