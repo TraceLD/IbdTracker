1f4180c4e91a8e8b5c0abcd051e6c8c711f5f5ff
Trace <lukaszdabrowski881@gmail.com>
2021-01-07 19:55:20 +0000

    add patient store

--- src/IbdTracker.Frontend/src/stores/authStore.ts
+++ src/IbdTracker.Frontend/src/stores/authStore.ts
@@ -1,7 +1,8 @@
+import type { Patient } from "../models/responses";
 import type { User } from "@auth0/auth0-spa-js";
 import { Writable, writable } from "svelte/store";
 
 export let isLoading: Writable<boolean> = writable(true);
 export let isAuthenticated: Writable<boolean> = writable(false);
 export let user: Writable<User> = writable(null);
-export let patient: any = writable(null);
+export let patient: Writable<Patient> = writable(null);
