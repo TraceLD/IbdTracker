d66610bf7e8a40fa22992842638aa78c04840db3
Trace <lukaszdabrowski881@gmail.com>
2021-01-08 09:18:21 +0000

    apply prescriptions migration

--- src/IbdTracker.Core/Entities/Prescription.cs
+++ src/IbdTracker.Core/Entities/Prescription.cs
@@ -6,6 +6,7 @@ namespace IbdTracker.Core.Entities
     {
         public Guid PrescriptionId { get; set; }
         public string Dosage { get; set; } = null!;
+        public DateTime EndDateTime { get; set; }
 
         public string PatientId { get; set; } = null!;
         
