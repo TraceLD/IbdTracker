b3ae25ce6187d619b132767215e7d74bcf2b6897
Trace <lukaszdabrowski881@gmail.com>
2021-04-05 15:58:15 +0100

    fix UI if there are no meals

--- src/IbdTracker.Frontend/src/pages/dashboard/food/index.svelte
+++ src/IbdTracker.Frontend/src/pages/dashboard/food/index.svelte
@@ -30,29 +30,33 @@
         </div>
     {/each}
 
-    <div class="flex">
-        <a
-            href={$url("./print")}
-            target="blank"
-            class="flex items-center py-2 px-4 ml-auto rounded-lg text-gray-100 bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                class="w-4 h-4 mr-1"
+    {#if res.length !== 0}
+        <div class="flex">
+            <a
+                href={$url("./print")}
+                target="blank"
+                class="flex items-center py-2 px-4 ml-auto rounded-lg text-gray-100 bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
             >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
-                />
-            </svg>
-            Print
-        </a>
-    </div>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    class="w-4 h-4 mr-1"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
+                    />
+                </svg>
+                Print QR codes
+            </a>
+        </div>
+    {:else}
+        <p>You have not added any meals yet.</p>
+    {/if}
 {:catch e}
     <Error errorMsg={e} />
 {/await}
