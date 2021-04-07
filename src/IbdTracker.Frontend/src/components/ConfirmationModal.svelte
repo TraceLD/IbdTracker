c7a512362983151ca4e7b006362e6772e4627e6e
Trace <lukaszdabrowski881@gmail.com>
2021-04-07 22:35:40 +0100

    fix spacing on mobile

--- src/IbdTracker.Frontend/src/components/ConfirmationModal.svelte
+++ src/IbdTracker.Frontend/src/components/ConfirmationModal.svelte
@@ -97,7 +97,7 @@
                 <button
                     on:click={onCancel}
                     type="button"
-                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 mb-2 sm:mb-0 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                 >
                     Cancel
                 </button>
