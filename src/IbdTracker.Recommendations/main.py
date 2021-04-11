d91600d8bc616c5f5cfa632685f575660bc63a58
Trace <lukaszdabrowski881@gmail.com>
2021-04-11 22:41:30 +0100

    add CORS

--- src/IbdTracker.Recommendations/main.py
+++ src/IbdTracker.Recommendations/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# only ASP.NET Core should be able to call this microservice over localhost.
+# Users should not be able to call it directly as it does not have auth.
+origins = [
+    "http://localhost"
+    "http://localhost:5000",
+    "https://localhost:5001"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/recommendations")
