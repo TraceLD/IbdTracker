66600d1b4fe57424ec05d1b9edbe269d5edc4809
Trace <lukaszdabrowski881@gmail.com>
2021-04-30 11:03:50 +0100

    add flare up detector UI

--- src/IbdTracker.Frontend/src/pages/dashboard/flareups/index.svelte
+++ src/IbdTracker.Frontend/src/pages/dashboard/flareups/index.svelte
@@ -38,34 +38,65 @@
                 bowel movements, we think it is unlikely that you are
                 experiencing a flare up currently.
             </p>
-        {:else}{/if}
+        {:else}
+            <div class="flex items-center mb-3">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-8 w-8 mr-1 text-red-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                </svg>
+                <h3 class="mb-0">Likely in a flare up</h3>
+            </div>
 
-        <div class="flex items-center mb-3">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-8 w-8 mr-1 text-red-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-            </svg>
-            <h3 class="mb-0">Likely in a flare up</h3>
-        </div>
-        <div class="">
-            <p>
-                Based on your recent (over the last 2 weeks) pain events and bowel
-                movements, we think it is likely that you are experiencing a flare
-                up currently.
-            </p>
-            <a href="/dashboard/appointments/add" class="underline">
-                We suggest you book an appointment with your doctor.
-            </a>
-        </div>
+            <div>
+                <p class="mb-2">
+                    Based on your recent (over the last 2 weeks) pain events and
+                    bowel movements, we think it is likely that you are
+                    experiencing a flare up currently.
+                </p>
+                <a href="/dashboard/appointments/add" class="underline">
+                    We suggest you book an appointment with your doctor.
+                </a>
+            </div>
+
+            <div class="mt-4">
+                <h4 class="text-lg font-semibold">Why?</h4>
+
+                {#if res.painEventsPerDay.actualValue > res.painEventsPerDay.threshold}
+                    <p class="mb-2">
+                        - You experience ~{res.painEventsPerDay.actualValue.toFixed(
+                            0
+                        )} pain events per day.
+                    </p>
+                {/if}
+
+                {#if res.bmsPerDay.actualValue > res.bmsPerDay.threshold}
+                    <p class="mb-2">
+                        - On average you have {res.bmsPerDay.actualValue.toFixed(
+                            0
+                        )}
+                        bowel movements per day. According to the NHS healthy amount
+                        is between twice a day and once every 2-3 days.
+                    </p>
+                {/if}
+
+                {#if res.bloodyBmsPercentage.actualValue > res.bloodyBmsPercentage.threshold}
+                    <p class="mb-2">
+                        - On average {res.bloodyBmsPercentage.actualValue.toFixed(
+                            0
+                        )}% of your bowel movements contain blood.
+                    </p>
+                {/if}
+            </div>
+        {/if}
     </div>
 {/await}
